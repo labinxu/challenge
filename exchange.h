750cac0fcd8121aba2ba9fefa4ecc43efb69df41
Labin.xu <flowinair@gmail.com>
2018-08-16 16:40:27 +0800

    Update exchange.h

--- exchange.h
+++ exchange.h
@@ -1,5 +1,25 @@
 #ifndef EXCHANGE_H
 #define EXCHANGE_H
+#ifndef EXCHANGE_H
+#define EXCHANGE_H
+#include <string>
+#include <map>
+#include <unistd.h>
+#include "order.h"
+
+typedef Order_t Order;
+class Exchange
+{
+ public:
+  std::string NewOrderConfirmed(const Order &order);
+  bool OrderCancelled(const std::string &exIdentify);
+  bool OrderTrade(const std::string &exIdentify);
+ private:
+  std::map<std::string, Order_t> _confirmedOrders;
+  std::map<std::string, Order_t> _cancelledOrders;
+  std::map<std::string, uint32_t> _tradedOrders;
+};
+#endif
 
 
 #endif
