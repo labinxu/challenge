160cb2ab9fd5308547693a5a14cb4e4028bb60b4
Labin.xu <flowinair@gmail.com>
2018-08-17 08:38:14 +0000

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -4,3 +4,50 @@ int main()
 {
     test_OrderIdentifier_Serialize();
 }
+
+#include <iostream>
+
+
+#include "../message.h"
+
+
+#include "../types.h"
+
+
+using namespace std;
+
+
+int main()
+
+
+{
+
+
+  //OrderIdentifier oi{1,2,3,4};
+
+
+  auto msg=new message<OrderIdentifier>(OrderIdentifier{1,2,3,4},message_type::CANCEL_ORDER);
+
+
+  
+
+
+  std::cout<<int(msg->msg_type())<<std::endl;
+
+
+　
+
+
+  auto msg1 = make_message<OrderIdentifier>(msg->data());
+
+
+  auto oi = msg1->get_value();
+
+
+  std::cout<<oi->_market<<oi->_desk<<oi->_trader<<oi->_sequence<<endl;
+
+
+　
+
+
+}
